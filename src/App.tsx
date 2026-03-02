2878370030d233dd4fb7580d82067b8d4539e8f0
himeshvats19 <himeshvats@Himeshs-MacBook-Pro.local>
2026-03-02 15:15:53 +0530

    Fixed build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Page from './components/image-seq'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Page/>
